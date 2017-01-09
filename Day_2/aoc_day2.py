55abcce8ce1dc63e370191caf469e41d61f2a4c6
Gabor <gabor.hajba@gmail.com>
2017-01-09 10:02:30 +0100

    day2 second part

--- Day_2/aoc_day2.py
+++ Day_2/aoc_day2.py
@@ -4,22 +4,31 @@ __author__ = "GHajba"
 __copyright__ = "Copyright 2016, JaPy Szoftver Kft"
 __license__ = 'MIT'
 
+keypad = [
+    [None, None, '1', None, None],
+    [None, '2', '3', '4', None],
+    ['5', '6', '7', '8', '9'],
+    [None, 'A', 'B', 'C', None],
+    [None, None, 'D', None, None],
+]
+
 if __name__ == '__main__':
     with open('input2.txt', 'r') as infile:
         lines = infile.readlines()
-        button = 5
+        row = 3
+        button = 0
         for l in lines:
             for c in l:
                 if c == 'R':
-                    if button in [3, 6, 9]: continue
+                    if button == 4 or keypad[row][button + 1] is None: continue
                     button += 1
                 if c == 'L':
-                    if button in [1, 4, 7]: continue
+                    if button == 0 or keypad[row][button - 1] is None: continue
                     button -= 1
                 if c == 'U':
-                    if button in [1, 2, 3]: continue
-                    button -= 3
+                    if row == 0 or keypad[row - 1][button] is None: continue
+                    row -= 1
                 if c == 'D':
-                    if button in [7, 8, 9]: continue
-                    button += 3
-            print(button)
+                    if row == 4 or keypad[row + 1][button] is None: continue
+                    row += 1
+            print(keypad[row][button])
