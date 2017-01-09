4eee9cce5d3a8da0205217a1eaa6513905e00680
Gabor <gabor.hajba@gmail.com>
2017-01-09 09:28:02 +0100

    finished Day 1 of AoC

---
+++ Day_2/README.md
@@ -0,0 +1,3 @@
+# Advent of Code -- Day 1
+
+Here is my solution to [Advent of Code 2016 Day 1](http://adventofcode.com/2016/day/1) puzzle in Python 3.
--- Day_2/aoc_day1.py
+++ Day_2/aoc_day1.py
@@ -9,18 +9,30 @@ y_direction = [1, 0, -1, 0]
 
 direction = 0
 
+locations = set()
+
 if __name__ == '__main__':
     with open('input1.txt') as infile:
         data = infile.readline().split(', ')
 
     x = 0
     y = 0
+    twice = []
 
     for info in data:
         if info[0] == 'L':
             direction = (direction + 3) % 4
         else:
             direction = (direction + 1) % 4
-        x += x_direction[direction] * int(info[1:])
-        y += y_direction[direction] * int(info[1:])
-    print(abs(x) + abs(y))
+        x_d = x_direction[direction]
+        y_d = y_direction[direction]
+
+        for i in range(1, int(info[1:]) + 1):
+            x += x_d
+            y += y_d
+            if (x, y) in locations:
+                twice.append(abs(x) + abs(y))
+                break
+            else:
+                locations.add((x, y))
+    print(twice[0])
