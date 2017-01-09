ee7327bddece49aa43538cedc5c2f3ed10760eba
Gabor <gabor.hajba@gmail.com>
2017-01-09 14:50:32 +0100

    day3 part two

--- Day_2/aoc_day3.py
+++ Day_2/aoc_day3.py
@@ -6,9 +6,25 @@ __license__ = 'MIT'
 
 if __name__ == '__main__':
     count = 0
-    with open('input3.txt','r') as infile:
+    with open('input3.txt', 'r') as infile:
+        t = [[], [], []]
+        l = 0
         for line in infile.readlines():
-            data = sorted([int(d) for d in line.split()])
-            if data[0] + data[1] > data[2]:
-                count += 1
+            if l > 0 and l % 3 == 0:
+                for i in range(3):
+                    data = sorted(t[i])
+                    if data[0] + data[1] > data[2]:
+                        count += 1
+                t = [[], [], []]
+            l += 1
+            x, y, z = [int(d) for d in line.split()]
+            t[0].append(x)
+            t[1].append(y)
+            t[2].append(z)
+        else:
+            for i in range(3):
+                data = sorted(t[i])
+                if data[0] + data[1] > data[2]:
+                    count += 1
+
     print(count)
