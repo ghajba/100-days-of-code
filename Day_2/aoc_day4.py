12e3680736e427d1405c03e2f74e5d2aa49ccdd2
Gabor <gabor.hajba@gmail.com>
2017-01-09 17:02:43 +0100

    day4 part two

--- Day_2/aoc_day4.py
+++ Day_2/aoc_day4.py
@@ -6,21 +6,22 @@ __author__ = "GHajba"
 __copyright__ = "Copyright 2016, JaPy Szoftver Kft"
 __license__ = 'MIT'
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 if __name__ == '__main__':
-    result = 0
-    with open('input4.txt','r') as infile:
+    with open('input4.txt', 'r') as infile:
         for line in infile.readlines():
             line = line.strip()
             last_dash = line.rfind('-')
             checksum_start = line.find('[')
-            checksum = line[checksum_start+1:-1]
-            number = int(line[last_dash+1:checksum_start])
-            c = sorted(Counter(line[:last_dash].replace('-','')).most_common(), key=lambda t: (-t[1], t[0]))
-            valid = True
-            for i in range(len(checksum)):
-                if c[i][0] != checksum[i]:
-                    valid = False
-                    break
-            if valid:
-                result += number
-    print(result)
+            room_name = line[:last_dash]
+            number = int(line[last_dash + 1:checksum_start])
+            decrypted = ''
+            for c in room_name:
+                if c == '-':
+                    decrypted += ' '
+                else:
+                    decrypted += alphabet[(alphabet.index(c) + number) % 26]
+            if 'north' in decrypted:
+                print(number)
+                break
